6d6590aa10927a7a6a4418ff4d6136ddb118ea13
devni-heraliyawala <devniheraliyawala314@gmail.com>
2022-07-19 21:46:55 +0530

    Add project files

---
+++ BlazorEcommerce/Server/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BlazorEcommerce.Server
+{
+    [Route("api/[controller]")]
+    public class ProductController : Controller
+    {
+        private readonly DataContext _context;
+
+        public ProductController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProduct()
+        {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+    }
+}
+
